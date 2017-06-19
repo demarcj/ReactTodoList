3c3f12d3582748ca29dfc19dedae3110050e806d
DeMarc Johnson <demarcjohnson@yahoo.com>
2017-06-19 08:32:00 -0400

    Change some css element and can do list from constructor object

--- src/App.js
+++ src/App.js
@@ -3,16 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn react', isComplete: true},
+        {id: 2, name: 'Start a great career', isComplete: false},
+        {id: 3, name: 'Live life!', isComplete: false}
+      ],
+      currentTodo: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange (evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>To Do List</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="todoApp">
+          <form action="">
+            <b><label htmlFor="todo">Add To Do!</label></b>
+            <input type="text" onChange={this.handleInputChange} id="todo" value={this.state.currentTodo}/>
+          </form>
+          <div className="todoList">
+            <ul>
+              {this.state.todos.map((todo) => <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}
+                </li>)}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
